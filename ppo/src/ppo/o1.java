077ecd364df2fc0b060d2a5905f320af12d465ca
Shree <Shree@Shree-PC>
2017-09-28 11:45:11 +0530

    ppo commit

---
+++ ppo/src/ppo/o1.java
@@ -0,0 +1,17 @@
+package ppo;
+
+import java.util.Scanner;
+
+public class o1 {
+	public static void main(String args[])
+	{
+		float a,b,c;
+		System.out.println("Enter two numbers:=");
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		b=in.nextInt();
+		c=a/b;
+		System.out.println("dividation:="+c);
+	}
+
+}
